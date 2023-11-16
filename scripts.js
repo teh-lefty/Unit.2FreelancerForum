aa2434e1d98480f8d7f2828ef2e95f28fb5bed4e
teh-lefty <102568631+teh-lefty@users.noreply.github.com>
2023-11-16 12:29:03 -0800

    testing

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const freelancers = [
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" }
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  {"Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver"},
+];
+
